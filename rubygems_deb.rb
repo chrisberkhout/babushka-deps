af5e97ff12d0a72c4ef810d009630e3b3a8cac83
Chris Berkhout <chrisberkhout@gmail.com>
2010-02-07 19:12:40 +1100

    Fixed 'rubygems deb' to add the rubygems deb bin directory to the (sudo) secure path.

--- rubygems_deb.rb
+++ rubygems_deb.rb
@@ -19,16 +19,17 @@ end
 dep 'rubygems deb path setup' do
   requires \
     'rubygems deb bin dir added to path in profile',
-    'rubygems deb bin dir in current path'
+    # 'rubygems deb bin dir in current path',
+    'rubygems deb bin dir in secure path'
 end
 
 
 dep 'rubygems deb bin dir added to path in profile' do
   requires 'rubygems deb package installed'
-  met? { File.exists?('/etc/profile.d/add_rubygems_bin_dir_to_path.sh') }
+  met? { File.exists?('/etc/profile.d/add_rubygems_deb_bin_dir_to_path.sh') }
   meet { 
     set :rubygems_deb_bin_dir, `gem env`.scan(/EXECUTABLE DIRECTORY: (.*)$/).first.first
-    render_erb "rubygems/add_rubygems_deb_bin_dir_to_path.sh.erb", :to => '/etc/profile.d/add_rubygems_deb_bin_dir_to_path.sh'
+    render_erb "rubygems_deb/add_rubygems_deb_bin_dir_to_path.sh.erb", :to => '/etc/profile.d/add_rubygems_deb_bin_dir_to_path.sh'
     shell "chmod +x /etc/profile.d/add_rubygems_deb_bin_dir_to_path.sh"
   }
 end
@@ -41,5 +42,26 @@ dep 'rubygems deb bin dir in current path' do
   }
   meet { 
     ENV['PATH'] = ENV['PATH'] + var(:rubygems_deb_bin_dir)
+  }
+end
+
+dep 'rubygems deb bin dir in secure path' do
+  # with ubuntu/debian's rubygems this has to be done for sudo to be able to use gem executables (e.g. rake)
+  requires 'rubygems deb package installed'
+  met? { 
+    dir = set :rubygems_deb_bin_dir, `gem env`.scan(/EXECUTABLE DIRECTORY: (.*)$/).first.first
+    `sudo env`[/^PATH=.*#{dir}.*$/].is_a?(String)
+  }
+  meet { 
+    current_path = `echo $PATH`.chomp
+    new_path = current_path.include?(var :rubygems_deb_bin_dir) ? current_path : current_path+":"+var(:rubygems_deb_bin_dir)
+    lines = File.new('/etc/sudoers').readlines
+    lines.insert(lines.index("Defaults\tenv_reset\n")+1, "Defaults\tsecure_path=\"#{new_path}\"\n")
+    while (File.exists?("/etc/sudoers.tmp")) do; puts "Waiting for /etc/sudoers.tmp"; sleep 1; end
+    File.open('/etc/sudoers.tmp', 'w') { |f| f.write(lines.join) }
+    File.chmod(0440, '/etc/sudoers.tmp')
+    `cp /etc/sudoers /etc/sudoers.bak`
+    # could do a check with visudo here to make sure that the new file can be parsed without errors... but it probably can.
+    `mv /etc/sudoers.tmp /etc/sudoers`
   }
 end
--- rubygems_handy_gems.rb
+++ rubygems_handy_gems.rb
@@ -6,11 +6,7 @@ end
 
 dep 'rake' do
   requires 'rubygems'
-  met? { 
-    set :rubygems_deb_bin_dir, `gem env`.scan(/EXECUTABLE DIRECTORY: (.*)$/).first.first
-    ENV['PATH'].include?(var :rubygems_deb_bin_dir)
-    `rake --version 2>&1`.include?('rake, version ') 
-  }
-  # met? { `gem list rake`.include?('rake ') && `rake --version 2>&1`.include?('rake, version ') }
-  # meet { sudo "gem install rake" }
+  met? { `gem list rake`.include?('rake ') && `rake --version 2>&1`.include?('rake, version ') }
+  meet { sudo "gem install rake" }
 end
+
