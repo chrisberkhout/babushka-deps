b7f68df040f9c804410db446f13d748c62f94c7b
Chris Berkhout <chrisberkhout@gmail.com>
2010-02-08 16:34:36 +1100

    Added comments.

--- ruby_src.rb
+++ ruby_src.rb
@@ -5,6 +5,9 @@ end
 
 dep 'ruby-1.8.7-p248' do
   requires 'build-essential'
+  # some claim this also requires an `apt-get install libssl-dev libreadline5-dev zlib1g-dev`
+  #   http://wiki.rubyonrails.org/getting-started/installation/linux
+  # could also use checkinstall so that it can be more easily uninstalled (see https://help.ubuntu.com/community/CheckInstall)
   met? { `ruby -v 2>&1`.include?("ruby 1.8.7 (2009-12-24 patchlevel 248)") }
   meet { 
     Dir.chdir '/usr/local/src'
