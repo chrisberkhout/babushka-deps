bc6d959cab73429b99744bfd05b1f0d502f05467
Chris Berkhout <chrisberkhout@gmail.com>
2010-07-07 00:05:28 +1000

    Dep for cloning a proj from a repo (no deploy keys tuff), and adding github's key to known_hosts.

--- system.rb
+++ system.rb
@@ -7,6 +7,7 @@ dep 'cb system base' do
     'cb ubuntu karmic 9.10 or later', # just because that's what I have been writing my deps on
     'cb time setup',
     'cb ssh server',
+    'cb known hosts',
     'cb handy sys packages',
     'cb ruby',
     'cb rubygems',
---
+++ system/known_hosts.rb
@@ -0,0 +1,13 @@
+dep 'cb known hosts' do
+  requires 'cb github in known hosts'
+end
+
+
+dep 'cb github in known hosts' do
+  met? { grep var(:github_key), var(:system_know_hosts) }
+  meet { append_to_file var(:github_key), var(:system_know_hosts), :sudo => true }
+  setup {
+    set :system_know_hosts, '/etc/ssh/ssh_known_hosts'
+    set :github_key, 'github.com,207.97.227.239 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=='
+  }
+end
