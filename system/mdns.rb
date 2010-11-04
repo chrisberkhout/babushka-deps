d8b774a58cfb87127b8f8f1a42056000c6d94f10
Chris Berkhout <chrisberkhout@gmail.com>
2010-11-04 15:38:57 +1100

    Fixed mDNS / avahi-daemon.

--- system/mdns.rb
+++ system/mdns.rb
@@ -6,12 +6,29 @@ dep 'mdns' do
 end
 
 dep 'avahi-daemon' do
-  met? { 
-    `dpkg -s avahi-daemon 2>&1`.include?("\nStatus: install ok installed\n") &&
-    `service avahi-daemon status 2>&1`.include?("avahi-daemon start/running")
-  }
+  requires \
+    'avahi-daemon installed',
+    'avahi-daemon configured',
+    'avahi-daemon running'
+end
+
+dep 'avahi-daemon installed' do
+  met? { `dpkg -s avahi-daemon 2>&1`.include?("\nStatus: install ok installed\n") }
+  meet { sudo "apt-get -y install avahi-daemon" }
+end
+
+dep 'avahi-daemon configured' do
+  # VMware doesn't seem to provide an IPv6 interface, so we don't want to publish AAAA (IPv6) records.
+  # Without this change `ssh -vvv user@guesthostname.local.` times out on a IPv6 attempt before using IPv4.
+  met? { grep /^publish\-aaaa\-on\-ipv4\=no$/, '/etc/avahi/avahi-daemon.conf' }
   meet { 
-    sudo "apt-get -y install avahi-daemon"
+    sudo "service avahi-daemon stop"
+    change_line '#publish-aaaa-on-ipv4=yes', 'publish-aaaa-on-ipv4=no', '/etc/avahi/avahi-daemon.conf' 
     sudo "service avahi-daemon start"
   }
+end
+
+dep 'avahi-daemon running' do
+  met? { `service avahi-daemon status 2>&1`.include?("avahi-daemon start/running") }
+  meet { sudo "service avahi-daemon start" }
 end
