64b1d4bcf4a6102895c75ecfd8061bca9689a8df
Chris Berkhout <chrisberkhout@gmail.com>
2010-11-05 22:35:02 +1100

    Minor fixes.

--- user/account.rb
+++ user/account.rb
@@ -8,18 +8,18 @@ end
 
 dep 'account with password' do
   met? { grep(/^#{var(:username)}:/, '/etc/passwd') }
-  meet { `adduser #{var(:username)} --gecos ""` }
+  meet { sudo 'adduser #{var(:username)} --gecos ""' }
 end
 
 dep 'member of admin' do
   requires 'admin group'
-  met? { members_of('admin').include(var :username) }
+  met? { members_of('admin').include?(var :username) }
   meet { sudo "usermod -aG admin #{var :username}" }
 end
 
 dep 'member of rvm' do
   # This will ensure the existance of the rvm group and :username's membership, even if system-wide rvm hasn't been installed.
   requires 'rvm group'
-  met? { members_of('rvm').include(var :username) }
+  met? { members_of('rvm').include?(var :username) }
   meet { sudo "usermod -aG rvm #{var :username}" }
 end
