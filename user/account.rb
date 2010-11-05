84ad3df7ef48d6afe4e1b8ffa7346a20280c594e
Chris Berkhout <chrisberkhout@gmail.com>
2010-11-05 22:38:12 +1100

    Minor fixes.

--- user/account.rb
+++ user/account.rb
@@ -8,7 +8,7 @@ end
 
 dep 'account with password' do
   met? { grep(/^#{var(:username)}:/, '/etc/passwd') }
-  meet { sudo "adduser #{var(:username)} --gecos \"\"" }
+  meet { `sudo adduser #{var(:username)} --gecos ""` }
 end
 
 dep 'member of admin' do
