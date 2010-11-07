24a02e26737c689a8af90a426477772cb1e74385
Chris Berkhout <chrisberkhout@gmail.com>
2010-11-07 19:21:03 +1100

    rvm system should be finished now.

--- system/rvm_system.rb
+++ system/rvm_system.rb
@@ -2,19 +2,31 @@ dep 'rvm system' do
   # http://rvm.beginrescueend.com/rvm/install/
   #
   # This does a system-wide (multi-user) rvm install. 
-  # Individual users can additionally have their own user installations.
   #
   # System-wide rvm requires users to be in the rvm group to have write access.
   # This dep adds root and any admins to the rvm group. Also see the dep 'member of rvm'.
   # 
+  # After installation, to use rvm immediately, without closing and restarting the shell, 
+  # you need to use special subshells to ensure...
+  #   that the shell environment is properly reloaded (`bash -lc "command"`), and
+  #   that the new membership of the rvm group is active (`sg rvm -c "command"`). E.g.:
+  #     `bash -lc "sg rvm -c \"rvm install ree-1.8.7-2010.02\""`          # install ree
+  #     `bash -lc "sg rvm -c \"rvm --default use ree-1.8.7-2010.02\""`    # set ree as the system default ruby
+  #
+  # Gems will usually be installed into custom gemsets later, while setting up each account/project,
+  # however, they can be installed without closing and restarting the shell, as follows:
+  #     `bash -lc "sg rvm -c \"rvm ree-1.8.7-2010.02@defualt gem install rake\""`    # install into default gemset of given ruby
+  #     `bash -lc "sg rvm -c \"rvm ree-1.8.7-2010.02 gemset create gemsetname\""`    # create new gemset for a given ruby
+  #     `bash -lc "sg rvm -c \"rvm ree-1.8.7-2010.02@gemsetname gem install rake\""` # install into specific gemset of given ruby
+  # Other variations may not work as expected and should be carefully tested.
+  #
   requires \
-    'apt rubygems',
-    'curl',                     # defined elsewhere
-    'build-essential',          # defined elsewhere
+    'curl',
+    'build-essential',
+    'apt rubygems',             # the rvm system-wide install script expects a system rubygems to exist
     #'sys libs for jruby',       # not needed unless using jruby
     #'sys libs for ironruby',    # not needed unless using ironruby
-    'sys libs for mri and ree',
-    'extra sys libs for ruby-head'
+    'sys libs for mri and ree'
 
   met? { 
     # This works if system-wide rvm has been installed, even if the shell hasn't been closed and reopened.
@@ -44,15 +56,9 @@ dep 'rvm system' do
     sudo "usermod -aG rvm root" # Add root to the rvm group.
     members_of('admin').each { |u| sudo "usermod -aG rvm #{u}" } # Add admins to the rvm group
 
-    # To use rvm without closing and restarting the shell, run the command in a bash -lc subshell:
-    #   `bash -lc "rvm install ree-1.8.7-2010.02"`
-    #   `bash -lc "rvm use ree-1.8.7-2010.02 --default"`
-
-    # does a ruby install need an sg?
-
-    # mention gem installation
   }
 end
+
 
 dep 'apt rubygems' do
   met? { `dpkg -s rubygems 2>&1`.include?("\nStatus: install ok installed\n") }
@@ -61,21 +67,41 @@ end
   
 
 dep 'sys libs for mri and ree' do
-  # this is according to the RVM install instructions
+  # This is according to the RVM install instructions
   requires \
     'bison',             # defined elsewhere
     'openssl',
-    'zlib1g-dev',        # defined elsewhere
-    'libreadline5-dev',  # defined elsewhere
-    'libssl-dev',        # defined elsewhere
+    'zlib1g-dev',
+    'libreadline5-dev',
+    'libssl-dev',
     'libsqlite3-dev',
     'sqlite3',
-    'libxml2-dev'
+    'libxml2-dev',
+    'extra sys libs for ruby-head'
 end
+
+dep 'extra sys libs for ruby-head' do
+  requires \
+    'subversion',
+    'autoconf'
+end
+
 
 dep 'openssl' do
   met? { `dpkg -s openssl 2>&1`.include?("\nStatus: install ok installed\n") }
   meet { sudo "apt-get -y install openssl" }
+end
+dep 'zlib1g-dev' do
+  met? { `dpkg -s zlib1g-dev 2>&1`.include?("\nStatus: install ok installed\n") }
+  meet { sudo "apt-get -y install zlib1g-dev" }
+end
+dep 'libreadline5-dev' do
+  met? { `dpkg -s libreadline5-dev 2>&1`.include?("\nStatus: install ok installed\n") }
+  meet { sudo "apt-get -y install libreadline5-dev" }
+end
+dep 'libssl-dev' do
+  met? { `dpkg -s libssl-dev 2>&1`.include?("\nStatus: install ok installed\n") }
+  meet { sudo "apt-get -y install libssl-dev" }
 end
 dep 'libsqlite3-dev' do
   met? { `dpkg -s libsqlite3-dev 2>&1`.include?("\nStatus: install ok installed\n") }
@@ -89,14 +115,6 @@ dep 'libxml2-dev' do
   met? { `dpkg -s libxml2-dev 2>&1`.include?("\nStatus: install ok installed\n") }
   meet { sudo "apt-get -y install libxml2-dev" }
 end
-
-
-dep 'extra sys libs for ruby-head' do
-  requires \
-    'subversion',
-    'autoconf'
-end
-
 dep 'subversion' do
   met? { `dpkg -s subversion 2>&1`.include?("\nStatus: install ok installed\n") }
   meet { sudo "apt-get -y install subversion" }
--- system/rvm_system_default_ree.rb
+++ system/rvm_system_default_ree.rb
@@ -2,18 +2,12 @@ dep 'rvm system ree default' do
   # http://rvm.beginrescueend.com/
   # http://www.rubyenterpriseedition.com/
   requires 'rvm system ree'
-  met? {
-    `ls -l /usr/local/rvm/bin/ruby 2>&1`[/.*?\-\> \/usr\/local\/rvm\/wrappers\/ree\-1\.8\.7\-2010\.02\/ruby/] &&
-    `ls -l /usr/local/rvm/bin/gem 2>&1`[/.*?\-\> \/usr\/local\/rvm\/wrappers\/ree\-1\.8\.7\-2010\.02\/gem/]
-  }
-  meet { 
-    sudo 'bash -lc "rvm --default use ree-1.8.7-2010.02"' 
-    # Takes effect immediately because PATH already includes location of the new links.
-  }
+  met? { `ls -l /usr/local/rvm/bin/ruby 2>&1`[/.*?\-\> \/usr\/local\/rvm\/wrappers\/ree\-1\.8\.7\-2010\.02\/ruby/] }
+  meet { shell 'bash -lc "sg rvm -c \"rvm --default use ree-1.8.7-2010.02\""' }
 end
 
 dep 'rvm system ree' do
   requires 'rvm system'
   met? { `bash -lc "rvm list"`[/\bree\-1\.8\.7\-2010\.02\b/] }
-  meet { sudo 'bash -lc "rvm install ree-1.8.7-2010.02"' }
+  meet { shell 'bash -lc "sg rvm -c \"rvm install ree-1.8.7-2010.02\""' }
 end
