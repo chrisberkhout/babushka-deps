5cefca56059cb48b7b901ff6a2b1ea6aa75de25a
Chris Berkhout <chrisberkhout@gmail.com>
2010-12-09 22:41:54 +1100

    render_erb_inline does padding now.

--- helpers/erb.rb
+++ helpers/erb.rb
@@ -5,7 +5,11 @@ def changed_from_erb?(destination_file, source_erb)
   IO.read(destination_file) != ERB.new(IO.read(erb_path_for(source_erb))).result(binding)
 end
 
-def render_erb_inline(file)
-  File.exist?(erb_path_for(file)) &&
-  ERB.new(IO.read(erb_path_for(file))).result(binding)
+def render_erb_inline(file, opts = {})
+  if File.exist?(erb_path_for(file)) do
+    lines = ERB.new(IO.read(erb_path_for(file))).result(binding).split("\n")
+    [1..(lines.length-1)].each { |i| lines[i] = sprintf("%#{opts[:indent]}s", "") + lines[i] } if lines.length > 1
+  else
+    raise "Can't do an inline render of '#{file}' because it doesn't exist!"
+  end
 end
