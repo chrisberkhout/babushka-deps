e822044c6b92ea812d7d865d9e0a141fb2b49c2e
Chris Berkhout <chrisberkhout@gmail.com>
2010-12-09 17:39:47 +1100

    changed_from_erb? helper now checks that destination exists.

--- helpers/erb.rb
+++ helpers/erb.rb
@@ -1,5 +1,6 @@
 require 'erb'
 
 def changed_from_erb?(destination_file, source_erb)
+  !File.exist?(destination_file) ||
   IO.read(destination_file) != ERB.new(IO.read(erb_path_for(source_erb))).result(binding)
 end
