fc847072a5ac7c4e16e9e9f14970d8e8db716f43
Chris Berkhout <chrisberkhout@gmail.com>
2010-12-09 22:47:58 +1100

    Back again.

--- helpers/erb.rb
+++ helpers/erb.rb
@@ -7,7 +7,7 @@ end
 
 def render_erb_inline(file, opts = {})
   if File.exist?(erb_path_for(file))
-    lines = ERB.new(IO.read(erb_path_for(file))).result(binding)
+    lines = ERB.new(IO.read(erb_path_for(file))).result(binding).split("\n")
     [1..(lines.length-1)].each { |i| lines[i] = sprintf("%#{opts[:indent]}s", "") + lines[i] } if lines.length > 1
     lines.join("\n")
   else
