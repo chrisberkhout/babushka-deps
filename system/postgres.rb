7c5506b166861ea98c1580eca42f70dcd27a3e58
Chris Berkhout <chrisberkhout@gmail.com>
2010-12-13 23:56:01 +1100

    postgres install seems to be done. Need to rerun to check it.

--- system/postgres.rb
+++ system/postgres.rb
@@ -13,20 +13,30 @@ dep 'postgres' do
     sudo 'tar -xzf postgresql-9.0.1.tar.gz'
     sudo 'rm postgresql-9.0.1.tar.gz'
     Dir.chdir 'postgresql-9.0.1'
+    
     config_cmd = <<-END_OF_CMD
       ./configure \
           --with-tcl \
           --with-perl \
-          --with-python \
+          --with-python PYTHON='/usr/bin/python3.1' \
           --with-openssl
     END_OF_CMD
     shell config_cmd
     shell 'make'
     sudo  'make install'
-    sudo  'adduser postgres --system --no-create-home --group --disabled-password --disabled-login'
+    
+    # there are problems is the postgres user doesn't have the bash shell
+    sudo  'adduser postgres --system --group --no-create-home --disabled-password --disabled-login --shell "/bin/bash"'
     sudo  'mkdir /usr/local/pgsql/data'
-    sudo  'chown postgres /usr/local/pgsql/data'
+    sudo  'chown -R postgres:postgres /usr/local/pgsql/data'
     sudo  '/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data', :as => 'postgres'
+    
+    render_erb "postgres/etc_init.d_postgres.erb", :to => '/etc/init.d/postgres', :sudo => true
+    sudo  'chmod +x /etc/init.d/postgres'
+    sudo  'update-rc.d postgres defaults'
+    
+    render_erb "postgres/etc_profile.d_postgres.sh.erb", :to => '/etc/profile.d/postgres.sh', :sudo => true
+    sudo  'chmod +x /etc/profile.d/postgres.sh'
   }
 end
 
@@ -34,20 +44,21 @@ end
 dep 'postgres basic sys libs' do
   requires \
     'zlib1g-dev',
-    'libreadline5-dev'
+    'libreadline5-dev',
+    'lsb-base'
 end
 
 
 dep 'postgres procedural langs' do
   requires \
-    'tcl8.4',      # For PL/Tcl
+    'tcl8.4-dev',  # For PL/Tcl
     'libperl-dev', # For PL/Perl
     'python3-dev'  # For PL/Python
 end
 
-dep 'tcl8.4' do
-  met? { `dpkg -s tcl8.4 2>&1`.include?("\nStatus: install ok installed\n") }
-  meet { sudo "apt-get -y install tcl8.4" }
+dep 'tcl8.4-dev' do
+  met? { `dpkg -s tcl8.4-dev 2>&1`.include?("\nStatus: install ok installed\n") }
+  meet { sudo "apt-get -y install tcl8.4-dev" }
 end
 dep 'libperl-dev' do
   met? { `dpkg -s libperl-dev 2>&1`.include?("\nStatus: install ok installed\n") }
