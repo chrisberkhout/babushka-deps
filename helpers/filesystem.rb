57372e2d4560276e335091c267da5b162a3e5461
Chris Berkhout <chrisberkhout@gmail.com>
2011-02-08 21:39:18 +0800

    Fixed typo.

--- helpers/filesystem.rb
+++ helpers/filesystem.rb
@@ -3,7 +3,7 @@ require 'etc'
 def owner_and_group?(file, user_and_group)
   user, group = user_and_group.split(':')
   Etc.getpwuid(File.stat(file).uid).name == user &&
-  Etc.getgruid(File.stat(file).gid).name == group
+  Etc.getgrgid(File.stat(file).gid).name == group
 end
 
 def dir_empty?(dir, opts = {})
