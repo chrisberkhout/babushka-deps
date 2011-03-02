8d2c94d12dce68cbae17d65efac7e7497f4ba6da
Chris Berkhout <chrisberkhout@gmail.com>
2011-03-02 18:21:15 +0800

    Fixed syntax error.

--- system/apt-get_update_today.rb
+++ system/apt-get_update_today.rb
@@ -4,7 +4,7 @@ dep 'apt-get update today' do
     (Time.now - File.stat(File.expand_path("~/.ran_apt-get_update")).mtime) < 24*60*60 
   }
   meet { 
-    sudo  "apt-get update && "
+    sudo  "apt-get update"
     shell "touch ~/.ran_apt-get_update"
   }
 end
