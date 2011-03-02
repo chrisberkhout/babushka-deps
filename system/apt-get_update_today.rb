cfec8a9bcc081156a054a5c7859f8419175d644b
Chris Berkhout <chrisberkhout@gmail.com>
2011-03-02 18:17:05 +0800

    Fixed apt-get update today to handle the flag file not being there.

--- system/apt-get_update_today.rb
+++ system/apt-get_update_today.rb
@@ -1,5 +1,8 @@
 dep 'apt-get update today' do
-  met? { (Time.now - File.stat(File.expand_path("~/.ran_apt-get_update")).mtime) < 24*60*60 }
+  met? { 
+    File.exists?(File.expand_path("~/.ran_apt-get_update")) &&
+    (Time.now - File.stat(File.expand_path("~/.ran_apt-get_update")).mtime) < 24*60*60 
+  }
   meet { 
     sudo  "apt-get update && "
     shell "touch ~/.ran_apt-get_update"
