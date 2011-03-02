5d29fcf28d582d001f9fb57c688d55ab3b72f775
Chris Berkhout <chrisberkhout@gmail.com>
2011-03-02 18:14:02 +0800

    Added something to run apt-get update if it hasn't already been run by this user today.

--- system.rb
+++ system.rb
@@ -5,6 +5,7 @@ end
 dep 'system base' do
   requires \
     'ubuntu lucid 10.04 or later', # just because that's what I have been writing my deps on
+    'apt-get update today',
     'admin group',
     'time setup',
     'mdns',
---
+++ system/apt-get_update_today.rb
@@ -0,0 +1,7 @@
+dep 'apt-get update today' do
+  met? { (Time.now - File.stat(File.expand_path("~/.ran_apt-get_update")).mtime) < 24*60*60 }
+  meet { 
+    sudo  "apt-get update && "
+    shell "touch ~/.ran_apt-get_update"
+  }
+end
