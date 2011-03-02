307eca0f654c9a47c0f5eb2fd621373791771b26
Chris Berkhout <chrisberkhout@gmail.com>
2011-03-02 17:29:42 +0800

    Adjusted name of mysql dev libs package.

--- system/mysql.rb
+++ system/mysql.rb
@@ -9,7 +9,7 @@ dep 'mysql-server-5.1' do
   meet { sudo "env DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server-5.1" }
 end
 
-dep 'libmysqlclient15-dev' do
-  met? { `dpkg -s libmysqlclient15-dev 2>&1`.include?("\nStatus: install ok installed\n") }
-  meet { sudo "apt-get -y install libmysqlclient15-dev" }
+dep 'libmysqlclient-dev' do
+  met? { `dpkg -s libmysqlclient-dev 2>&1`.include?("\nStatus: install ok installed\n") }
+  meet { sudo "apt-get -y install libmysqlclient-dev" }
 end
