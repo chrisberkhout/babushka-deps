02b9d1d257a3d872d762c5e1e663f40720509628
Chris Berkhout <chrisberkhout@gmail.com>
2011-03-08 12:00:21 +0800

    Added imagemagick libraries.

--- system/handy_sys_packages.rb
+++ system/handy_sys_packages.rb
@@ -23,6 +23,15 @@ dep 'curl' do
 end
 
 dep 'imagemagick' do
+  requires \
+    'imagemagick deb',
+    'libmagickcore-dev'
+end
+dep 'imagemagick deb' do
   met? { `dpkg -s imagemagick 2>&1`.include?("\nStatus: install ok installed\n") }
   meet { sudo "apt-get -y install imagemagick" }
+end
+dep 'libmagickcore-dev' do
+  met? { `dpkg -s libmagickcore-dev 2>&1`.include?("\nStatus: install ok installed\n") }
+  meet { sudo "apt-get -y install libmagickcore-dev" }
 end
