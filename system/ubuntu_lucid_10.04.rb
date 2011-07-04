da3008c7990c8e625fd75da83b47d46e4c6feef3
Chris Berkhout <chrisberkhout@gmail.com>
2011-07-04 22:56:50 +0800

    Test

--- system/ubuntu_lucid_10.04.rb
+++ system/ubuntu_lucid_10.04.rb
@@ -3,5 +3,5 @@ dep 'ubuntu lucid 10.04' do
 end
 
 dep 'ubuntu lucid 10.04 or later' do
-  met? { `cat /etc/lsb-releaseNONO`[/DISTRIB\_RELEASE\=(\d+\.\d+)/m, 1].to_f >= 10.04 }
+  met? { `cat /etc/lsb-release`[/DISTRIB\_RELEASE\=(\d+\.\d+)/m, 1].to_f >= 10.04 }
 end
