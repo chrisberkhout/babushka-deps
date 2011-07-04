f254144bd6c9ab5dc4441a486032f7c82a5ce165
Chris Berkhout <chrisberkhout@gmail.com>
2011-07-04 22:56:26 +0800

    Test

--- system/ubuntu_lucid_10.04.rb
+++ system/ubuntu_lucid_10.04.rb
@@ -1,7 +1,7 @@
 dep 'ubuntu lucid 10.04' do
-  met? { grep 'Ubuntu 10.04', '/etc/lsb-releaseNONO' }
+  met? { grep 'Ubuntu 10.04', '/etc/lsb-release' }
 end
 
 dep 'ubuntu lucid 10.04 or later' do
-  met? { `cat /etc/lsb-release`[/DISTRIB\_RELEASE\=(\d+\.\d+)/m, 1].to_f >= 10.04 }
+  met? { `cat /etc/lsb-releaseNONO`[/DISTRIB\_RELEASE\=(\d+\.\d+)/m, 1].to_f >= 10.04 }
 end
