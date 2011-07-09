c8a4526c5d84c42e3fcbeaf32c6aeac7352a3fcb
Chris Berkhout <chrisberkhout@gmail.com>
2011-07-09 18:30:38 +0800

    Fixed requires in the wrong place.

--- system/nodejs.rb
+++ system/nodejs.rb
@@ -9,12 +9,12 @@ end
 
 
 dep "python-software-properties" do
-  requires "python-software-properties"
   met? { `dpkg -s python-software-properties 2>&1`.include?("\nStatus: install ok installed\n") }
   meet { sudo "apt-get -y install python-software-properties" }
 end
 
 dep "apt source chris-lea-node.js-lucid.list added" do
+  requires "python-software-properties"
   met? { File.exist?("/etc/apt/sources.list.d/chris-lea-node.js-lucid.list") }
   meet {
     sudo "add-apt-repository ppa:chris-lea/node.js"
