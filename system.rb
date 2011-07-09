05ba2d4c4855c01b78c6b56fd75c4ab79e387a1c
Chris Berkhout <chrisberkhout@gmail.com>
2011-07-09 18:13:46 +0800

    Added nodejs.

--- system.rb
+++ system.rb
@@ -14,6 +14,7 @@ dep 'system chrisberkhout.com' do
     # 'mysql',
     # 'sphinx',
     'postgres',
+    # 'nodejs',
     # 'mongodb',
     # 'elasticsearch',
     'nothing else'
@@ -35,6 +36,7 @@ dep 'system policycn' do
     # 'mysql',
     # 'sphinx',
     # 'postgres',
+    'nodejs',
     'mongodb',
     'elasticsearch',
     'nothing else'
--- system/nodejs.rb
+++ system/nodejs.rb
@@ -1,3 +1,29 @@
-# https://github.com/sstephenson/execjs
-# https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
-# https://github.com/joyent/node/wiki/Installation
+dep "nodejs" do
+  # https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
+  # https://github.com/joyent/node/wiki/Installation
+  requires \
+    "python-software-properties",
+    "apt source chris-lea-node.js-lucid.list added",
+    "nodejs deb"
+end
+
+
+dep "python-software-properties" do
+  requires "python-software-properties"
+  met? { `dpkg -s python-software-properties 2>&1`.include?("\nStatus: install ok installed\n") }
+  meet { sudo "apt-get -y install python-software-properties" }
+end
+
+deb "apt source chris-lea-node.js-lucid.list added" do
+  met? { File.exist?("/etc/apt/sources.list.d/chris-lea-node.js-lucid.list") }
+  meet {
+    sudo "add-apt-repository ppa:chris-lea/node.js"
+    sudo "apt-get update"
+  }
+end
+
+dep "nodejs deb" do
+  requires "apt source chris-lea-node.js-lucid.list added"
+  met? { `dpkg -s nodejs 2>&1`.include?("\nStatus: install ok installed\n") }
+  meet { sudo "apt-get -y install nodejs" }
+end
