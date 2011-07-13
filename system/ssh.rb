bc1c6a9e2b5e912939548fc083f8cf8d46354221
Chris Berkhout <chrisberkhout@gmail.com>
2011-07-13 18:45:11 +0800

    Added a comment to explain new dep.

--- system/ssh.rb
+++ system/ssh.rb
@@ -4,6 +4,8 @@ dep 'ssh server' do
 end
 
 dep 'ssh gateway ports enabled' do
+  # This makes it possible to open forwarded ports on all interfaces instead of just loopback.
+  # See "RemoteForward" in: http://souptonuts.sourceforge.net/sshtips.htm
   met? { grep /^GatewayPorts\s+yes/, "/etc/ssh/sshd_config" }
   meet { 
     append_to_file "GatewayPorts yes", "/etc/ssh/sshd_config", :sudo => true 
