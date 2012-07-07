8cc45d4f8f4598610194b627627719ab8bf61b32
Chris Berkhout <chrisberkhout@gmail.com>
2012-07-07 16:18:45 +1000

    Removed chinapolicy/policycn stuff.

--- system.rb
+++ system.rb
@@ -20,24 +20,3 @@ dep 'system chrisberkhout.com' do
     'nothing else'
 end
 
-dep 'system policycn' do
-  requires \
-    'ubuntu lucid 10.04 or later',
-    'apt-get update today',
-    'admin group',
-    'time setup',
-    'mdns',
-    'ssh server',
-    'known hosts',
-    'handy sys packages',
-    'rvm system ruby19 default',
-    'nginx',
-    # 'php',
-    # 'mysql',
-    # 'sphinx',
-    # 'postgres',
-    'nodejs',
-    'mongodb',
-    'elasticsearch',
-    'nothing else'
-end
